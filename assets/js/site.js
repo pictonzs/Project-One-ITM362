28282760b9df9703c340754fa2749f04d917c997
pictonzs <70493402+pictonzs@users.noreply.github.com>
2021-03-15 19:43:00 -0500

    Update js for new id name

--- assets/js/site.js
+++ assets/js/site.js
@@ -5,12 +5,21 @@ jQuery(function($) {
 });
 
 document.getElementById("name").addEventListener("click", myFunction);
+document.getElementById("email").addEventListener("click", myFunction2);
 function myFunction() {
   if (document.getElementById("name").className == "inactive") {
     document.getElementById("name").className ="active";
   }
   else {
     document.getElementById("name").className ="inactive";
+  }
+}
+function myFunction2() {
+  if (document.getElementById("email").className == "inactive") {
+    document.getElementById("email").className ="active";
+  }
+  else {
+    document.getElementById("email").className ="inactive";
   }
 }
   }
