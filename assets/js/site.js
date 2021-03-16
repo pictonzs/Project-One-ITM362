93a222e71f40fd2c8376dbac60875187769b5546
pictonzs <70493402+pictonzs@users.noreply.github.com>
2021-03-15 19:43:06 -0500

    Update js for new id name

--- assets/js/site.js
+++ assets/js/site.js
@@ -6,6 +6,7 @@ jQuery(function($) {
 
 document.getElementById("name").addEventListener("click", myFunction);
 document.getElementById("email").addEventListener("click", myFunction2);
+document.getElementById("phone").addEventListener("click", myFunction3);
 function myFunction() {
   if (document.getElementById("name").className == "inactive") {
     document.getElementById("name").className ="active";
@@ -22,5 +23,11 @@ function myFunction2() {
     document.getElementById("email").className ="inactive";
   }
 }
+function myFunction3() {
+  if (document.getElementById("phone").className == "inactive") {
+    document.getElementById("phone").className ="active";
+  }
+  else {
+    document.getElementById("phone").className ="inactive";
   }
 }
